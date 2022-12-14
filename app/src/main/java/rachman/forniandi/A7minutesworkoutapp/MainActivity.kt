1cccf6065472f96ee3f832e053eb68cff60c5549
RachmanForniandi <rachmanforniandi@gmail.com>
2022-12-14 15:57:00 +0700

    remove action bar & start to enabling viewbinding done.

--- app/src/main/java/rachman/forniandi/A7minutesworkoutapp/MainActivity.kt
+++ app/src/main/java/rachman/forniandi/A7minutesworkoutapp/MainActivity.kt
@@ -2,10 +2,24 @@ package rachman.forniandi.A7minutesworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import rachman.forniandi.A7minutesworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Start the exercise !!",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
